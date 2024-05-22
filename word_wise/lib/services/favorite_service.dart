9d8ba96e9148c214eb44baf4c38c8cd6db702b2c
Jeilson Araujo <jeilsonaraujo@icloud.com>
2024-05-22 14:16:03 -0300

    feat: add favorite service

--- word_wise/lib/services/favorite_service.dart
+++ word_wise/lib/services/favorite_service.dart
@@ -1,12 +1,29 @@
-import 'package:hive/hive.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class FavoriteService {
-  static get favoritesBoxName => 'favoritesWords';
-  Box hiveBox;
+  final SupabaseClient supabaseClient;
 
-  FavoriteService({required this.hiveBox});
+  FavoriteService({required this.supabaseClient});
 
-  Future<void> setFavorite({required String word, required bool isFavorite}) async {}
+  Future<void> addFavorite({required String word, required String userId}) async {
+    final data = await supabaseClient.from('favorites').select('*').eq('user_id', userId).eq('word', word);
+    if (data.isEmpty) {
+      await supabaseClient.from('favorites').insert({'word': word, 'user_id': userId});
+    }
+  }
 
-  Future<void> getFavorites({required String word, required bool isFavorite}) async {}
+  Future<void> removeFavorite({required String word, required String userId}) async {
+    final data = await supabaseClient.from('favorites').select('id').eq('user_id', userId).eq('word', word);
+    if (data.isNotEmpty) {
+      final id = data.first['id'];
+      await supabaseClient.from('favorites').delete().match({'id': id});
+    }
+  }
+
+  Future<List<String>> getFavorites({required String userId}) async {
+    final data = await supabaseClient.from('favorites').select('word').eq('user_id', userId);
+    final result = data.map((e) => e['word'] as String? ?? '').toList();
+
+    return result;
+  }
 }
