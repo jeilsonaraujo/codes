104d80a67acca83fb685d7318cee1093c772e6c1
Jeilson Araujo <jeilsonaraujo@icloud.com>
2024-05-22 10:49:52 -0300

    feat: add supabase to project to retrieve word lists, alowwing pagination, store favorites and deal with authentication, setup env file to store sensible data

--- word_wise/lib/core/inject.dart
+++ word_wise/lib/core/inject.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:word_wise/core/logger.dart';
 import 'package:word_wise/repositories/word_repository.dart';
 import 'package:word_wise/services/api_service.dart';
@@ -13,6 +15,24 @@ Future<void> setupInjection() async {
   await inject.reset();
 
   final cache = await CacheService.setupHive();
+
+  String _supabaseUrl = '';
+  String _supabaseAnonKey = '';
+  const String fileName = '.env';
+
+  try {
+    await dotenv.load(fileName: fileName);
+    _supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+    _supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+    if (_supabaseUrl.isEmpty) WWLogger.w(message: '"SUPABASE_URL" was not found on ".env" file');
+    if (_supabaseAnonKey.isEmpty) WWLogger.w(message: '"SUPABASE_ANON_KEY" was not found on ".env" file');
+  } catch (exception) {
+    WWLogger.e(message: 'Fail to load environment keys, file: "$fileName" not found!', exception: exception);
+  }
+
+  await Supabase.initialize(url: _supabaseUrl, anonKey: _supabaseAnonKey);
+
+  inject.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
 
   inject.registerLazySingleton<CacheService>(() => CacheService(hiveBox: cache));
 
